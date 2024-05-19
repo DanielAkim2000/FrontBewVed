d49b9b8bf59ab3c94be87f0366dbd836fda38ed9
DanielAkim2000 <emanedanielakim@gmail.com>
2024-05-19 18:37:13 +0200

    fix url axios.api

--- src/api/axios.api.jsx
+++ src/api/axios.api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api =  axios.create({
-    baseURL:'https://bewved-2ed900875474.herokuapp.com/'
+    baseURL:'https://bewved-4efa698bf3e0.herokuapp.com/'
 })
 
 export default api
