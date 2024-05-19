1a62425b669bb88e31e4b326296853414b41fa0e
DanielAkim2000 <emanedanielakim@gmail.com>
2024-05-19 18:54:11 +0200

    fix

--- src/api/axios.api.jsx
+++ src/api/axios.api.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api =  axios.create({
-    baseURL:'https://bewved-4efa698bf3e0.herokuapp.com/'
+    baseURL:'https://bewved-4efa698bf3e0.herokuapp.com/',
+    headers: {
+        'Content-Type': 'application/json',
+    }
 })
 
 export default api
